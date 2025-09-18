3601c34fc267cf34f83a4611fac00e4c9faa32e5
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-09-18 12:31:56 -0700

    Make tests' limits adjust to class variable

--- tests/test_qc/harp/test_environment_sensor.py
+++ tests/test_qc/harp/test_environment_sensor.py
@@ -49,8 +49,16 @@ class MockHarpDevice:
             )
             time_index = np.linspace(0, 10, 1000)
             np.random.seed(0)
-            temp = np.random.uniform(22, 28, len(time_index))
-            humidity = np.random.uniform(40, 60, len(time_index))
+            temp = np.random.uniform(
+                HarpEnvironmentSensorTestSuite.temperature_limit[0],
+                HarpEnvironmentSensorTestSuite.temperature_limit[1],
+                len(time_index),
+            )
+            humidity = np.random.uniform(
+                HarpEnvironmentSensorTestSuite.humidity_limit[0],
+                HarpEnvironmentSensorTestSuite.humidity_limit[1],
+                len(time_index),
+            )
             sensor_df = pd.DataFrame(
                 {
                     "Temperature": temp,
@@ -85,11 +93,21 @@ def mock_env_sensor_device_bad_temp():
     time_index = np.linspace(0, 10, 1000)
     temp = np.concatenate(
         [
-            np.random.uniform(10, 15, 500),  # too low
-            np.random.uniform(35, 40, 500),  # too high
+            np.random.uniform(
+                HarpEnvironmentSensorTestSuite.temperature_limit[0] * 0.1,
+                HarpEnvironmentSensorTestSuite.temperature_limit[0] * 0.9,
+                500,
+            ),  # too low
+            np.random.uniform(
+                HarpEnvironmentSensorTestSuite.temperature_limit[1] * 1.1,
+                HarpEnvironmentSensorTestSuite.temperature_limit[1] * 1.2,
+                500,
+            ),  # too high
         ]
     )
-    humidity = np.random.uniform(40, 60, 1000)
+    humidity = np.random.uniform(
+        HarpEnvironmentSensorTestSuite.humidity_limit[0], HarpEnvironmentSensorTestSuite.humidity_limit[1], 1000
+    )
     sensor_df = pd.DataFrame(
         {"Temperature": temp, "Humidity": humidity, "MessageType": ["EVENT"] * 1000},
         index=pd.Index(time_index, name="Seconds"),
@@ -102,11 +120,21 @@ def mock_env_sensor_device_bad_temp():
 def mock_env_sensor_device_bad_humidity():
     device = MockHarpDevice(whoami=1401)
     time_index = np.linspace(0, 10, 1000)
-    temp = np.random.uniform(22, 28, 1000)
+    temp = np.random.uniform(
+        HarpEnvironmentSensorTestSuite.temperature_limit[0], HarpEnvironmentSensorTestSuite.temperature_limit[1], 1000
+    )
     humidity = np.concatenate(
         [
-            np.random.uniform(10, 20, 500),  # too low
-            np.random.uniform(80, 90, 500),  # too high
+            np.random.uniform(
+                HarpEnvironmentSensorTestSuite.humidity_limit[0] * 0.1,
+                HarpEnvironmentSensorTestSuite.humidity_limit[0] * 0.9,
+                500,
+            ),  # too low
+            np.random.uniform(
+                HarpEnvironmentSensorTestSuite.humidity_limit[1] * 1.1,
+                HarpEnvironmentSensorTestSuite.humidity_limit[1] * 1.2,
+                500,
+            ),  # too high
         ]
     )
     sensor_df = pd.DataFrame(
@@ -140,7 +168,7 @@ class TestHarpEnvironmentSensorTestSuite:
 
         suite = HarpEnvironmentSensorTestSuite(mock_env_sensor_device_bad_temp)
         result = suite.test_temperature_within_expected_limits()
-        assert result.status == Status.FAILED
+        assert result.status == Status.WARNING
 
     def test_humidity_within_expected_limits(self, mock_env_sensor_device, mock_env_sensor_device_bad_humidity):
         suite = HarpEnvironmentSensorTestSuite(mock_env_sensor_device)
@@ -153,4 +181,4 @@ class TestHarpEnvironmentSensorTestSuite:
 
         suite = HarpEnvironmentSensorTestSuite(mock_env_sensor_device_bad_humidity)
         result = suite.test_humidity_within_expected_limits()
-        assert result.status == Status.FAILED
+        assert result.status == Status.WARNING
